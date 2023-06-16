6f2d501c2177d6ebd8d20962440419651a3170c5
Dom Binks <dominic.binks@gmail.com>
2023-06-16 20:38:38 +0100

    Add map to index.html
    A map has been added to index.html using JS and OpenStreetMap

--- main.go
+++ main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
-func main () {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+func main() {
+	/*
+		r := gin.Default()
+		r.GET("/ping", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+		r.Run()
+	*/
+
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"content": "This is an index page...",
 		})
 	})
-	r.Run()
+
+	router.Run("localhost:8080")
 }
