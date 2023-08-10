d3101560ae95259316f9fd62981c9880db53b42e
Dom Binks <dominic.binks@gmail.com>
2023-08-10 01:58:05 +0100

    Fix bug when switching to about page
    Fixed the bug that means when you switch to the about page, when you
    switch back to the map, the language checkbox never becomes disabled
    after loading in all the layers for that language.

--- front-end/src/app/about-page/about-page.component.ts
+++ front-end/src/app/about-page/about-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
+import { MapLayersService } from '../map-layers.service';
 
 @Component({
   selector: 'app-about-page',
@@ -12,6 +13,19 @@ import { HeaderComponent } from '../header/header.component';
   templateUrl: './about-page.component.html',
   styleUrls: ['./about-page.component.css']
 })
-export class AboutPageComponent {
 
+export class AboutPageComponent {
+  // When the page loads
+  ngOnInit() {
+    // Remove all the current layers on the map
+    this.layersService.layers = [];
+
+    // For each list of layers for each language on the map
+    for (let [key, value] of this.layersService.layersMap) {
+      // Remove all the layers from the list
+      this.layersService.layersMap.set(key, []);
+    }
+  }
+
+  constructor(private layersService: MapLayersService) {}
 }
